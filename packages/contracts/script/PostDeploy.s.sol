53db6def9e29722e2da2202ea47eee8baa71128e
naikkuma <carlos.escom.ipn@gmail.com>
2024-08-07 19:46:01 -0600

    contracts: + water terrain

--- packages/contracts/script/PostDeploy.s.sol
+++ packages/contracts/script/PostDeploy.s.sol
@@ -21,39 +21,40 @@ contract PostDeploy is Script {
     vm.startBroadcast(deployerPrivateKey);
 
     TerrainType O = TerrainType.None;
+    TerrainType W = TerrainType.Water;
     TerrainType T = TerrainType.TallGrass;
     TerrainType B = TerrainType.Boulder;
 
-    TerrainType[20][20] memory map = [
-      [O, O, O, O, O, O, T, O, O, O, O, O, O, O, O, O, O, O, O, O],
-      [O, O, T, O, O, O, O, O, T, O, O, O, O, B, O, O, O, O, O, O],
-      [O, T, T, T, T, O, O, O, O, O, O, O, O, O, O, T, T, O, O, O],
-      [O, O, T, T, T, T, O, O, O, O, B, O, O, O, O, O, T, O, O, O],
-      [O, O, O, O, T, T, O, O, O, O, O, O, O, O, O, O, O, T, O, O],
-      [O, O, O, B, B, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O],
-      [O, T, O, O, O, B, B, O, O, O, O, T, O, O, O, O, O, B, O, O],
-      [O, O, T, T, O, O, O, O, O, T, O, B, O, O, T, O, B, O, O, O],
-      [O, O, T, O, O, O, O, T, T, T, O, B, B, O, O, O, O, O, O, O],
-      [O, O, O, O, O, O, O, T, T, T, O, B, T, O, T, T, O, O, O, O],
-      [O, B, O, O, O, B, O, O, T, T, O, B, O, O, T, T, O, O, O, O],
-      [O, O, B, O, O, O, T, O, T, T, O, O, B, T, T, T, O, O, O, O],
-      [O, O, B, B, O, O, O, O, T, O, O, O, B, O, T, O, O, O, O, O],
-      [O, O, O, B, B, O, O, O, O, O, O, O, O, B, O, T, O, O, O, O],
-      [O, O, O, O, B, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O],
-      [O, O, O, O, O, O, O, O, O, O, B, B, O, O, T, O, O, O, O, O],
-      [O, O, O, O, T, O, O, O, T, B, O, O, O, T, T, O, B, O, O, O],
-      [O, O, O, T, O, T, T, T, O, O, O, O, O, T, O, O, O, O, O, O],
-      [O, O, O, T, T, T, T, O, O, O, O, T, O, O, O, T, O, O, O, O],
-      [O, O, O, O, O, T, O, O, O, O, O, O, O, O, O, O, O, O, O, O]
+    TerrainType[20][20] memory map_x00_y00 = [
+      [W, W, W, W, O, O, T, O, O, O, O, O, O, O, O, O, O, O, O, O],
+      [W, W, W, O, O, O, O, O, T, O, O, O, O, B, O, O, O, O, O, O],
+      [W, W, W, T, T, O, O, O, O, O, O, O, O, O, O, T, T, O, O, O],
+      [W, W, T, T, T, T, O, O, O, O, B, O, O, O, O, O, T, O, O, O],
+      [W, O, O, O, T, T, O, O, O, O, O, O, O, O, O, O, O, T, O, O],
+      [W, O, O, B, B, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O],
+      [W, W, O, O, O, B, B, O, O, O, O, T, O, O, O, O, O, B, O, O],
+      [W, W, T, T, O, O, O, O, O, T, O, B, O, O, T, O, B, O, O, O],
+      [W, W, T, O, O, O, O, T, T, T, O, B, B, O, O, O, O, O, O, O],
+      [W, O, O, O, O, O, O, T, T, T, O, B, T, O, T, T, O, O, O, O],
+      [W, W, O, O, O, B, O, O, T, T, O, B, O, O, T, T, O, O, O, O],
+      [W, W, W, W, O, O, T, O, T, T, O, O, B, T, T, T, O, O, O, O],
+      [W, W, W, W, O, O, O, O, T, O, O, O, B, O, T, O, O, O, O, O],
+      [W, W, O, B, B, O, O, O, O, O, O, O, O, B, O, T, O, O, O, O],
+      [W, W, O, O, B, O, O, O, O, O, O, O, O, O, O, O, O, O, O, O],
+      [W, W, O, O, O, O, O, O, O, O, B, B, O, O, T, O, O, O, O, O],
+      [W, W, O, O, T, O, O, O, T, B, O, O, O, T, T, O, B, O, O, O],
+      [W, W, W, W, W, W, W, W, O, O, O, O, O, T, O, O, O, O, O, O],
+      [W, W, W, W, W, W, W, W, O, O, O, T, O, O, O, T, O, O, O, O],
+      [W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W, W]
     ];
 
-    uint32 height = uint32(map.length);
-    uint32 width = uint32(map[0].length);
+    uint32 height = uint32(map_x00_y00.length);
+    uint32 width = uint32(map_x00_y00[0].length);
     bytes memory terrain = new bytes(width * height);
 
     for (uint32 y = 0; y < height; y++) {
       for (uint32 x = 0; x < width; x++) {
-        TerrainType terrainType = map[y][x];
+        TerrainType terrainType = map_x00_y00[y][x];
         if (terrainType == TerrainType.None) continue;
 
         terrain[(y * width) + x] = bytes1(uint8(terrainType));
