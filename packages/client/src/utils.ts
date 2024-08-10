b406ceaa9add96999891eb7b51bdde36f1092dfd
pryority <matthewapryor@gmail.com>
2024-08-10 18:44:16 -0400

    ✨ Add helper function to change rotation of player

--- packages/client/src/utils.ts
+++ packages/client/src/utils.ts
@@ -1,3 +1,4 @@
+import { Direction } from "./direction";
 import { TerrainType } from "./terrainTypes";
 
 export function getTerrainTypeByEmoji(emoji: string | undefined): TerrainType {
@@ -12,5 +13,20 @@ export function getTerrainTypeByEmoji(emoji: string | undefined): TerrainType {
       return TerrainType.Ice;
     default:
       return TerrainType.None;
+  }
+}
+
+export function getAngle(direction: Direction) {
+  switch (direction) {
+    case Direction.North:
+      return "rotate-180"; // Facing up
+    case Direction.South:
+      return "rotate-0"; // Facing down
+    case Direction.East:
+      return "-rotate-90"; // Facing right
+    case Direction.West:
+      return "rotate-90"; // Facing left
+    default:
+      return "rotate-0"; // Default (no rotation)
   }
 }
