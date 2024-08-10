1630f454f9a6ff7182300212c214ac5ec312124e
pryority <matthewapryor@gmail.com>
2024-08-10 16:53:07 -0400

    ✨ Add new conditions for start/game/win state

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,12 +1,15 @@
+import React, { useState } from "react";
 import { useComponentValue } from "@latticexyz/react";
 import { SyncStep } from "@latticexyz/store-sync";
 import { singletonEntity } from "@latticexyz/store-sync/recs";
 import { useMUD } from "./MUDContext";
 import { GameBoard } from "./GameBoard";
+import WinningScreen from "./WinningScreen"; // Import WinningScreen
 
 export const App = () => {
   const {
     components: { SyncProgress },
+    systemCalls: { spawn, respawn },
   } = useMUD();
 
   const loadingState = useComponentValue(SyncProgress, singletonEntity, {
@@ -17,14 +20,39 @@ export const App = () => {
     lastBlockNumberProcessed: 0n,
   });
 
+  const [gameStarted, setGameStarted] = useState(false);
+  const [hasWon, setHasWon] = useState(false);
+
+  const handleStartGame = () => {
+    setGameStarted(true);
+    setHasWon(false);
+    spawn(0, 0);
+  };
+
+  const handleWinGame = () => {
+    setHasWon(true);
+  };
+
+  const handleRestartGame = async () => {
+    setGameStarted(false);
+    setHasWon(false);
+    respawn(0, 0);
+  };
+
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       {loadingState.step !== SyncStep.LIVE ? (
         <div>
           {loadingState.message} ({loadingState.percentage.toFixed(2)}%)
         </div>
+      ) : hasWon ? (
+        <WinningScreen onRestartGame={handleRestartGame} />
+      ) : !gameStarted ? (
+        <div>
+          <button onClick={handleStartGame}>Start Game</button>
+        </div>
       ) : (
-        <GameBoard />
+        <GameBoard onWinGame={handleWinGame} />
       )}
     </div>
   );
